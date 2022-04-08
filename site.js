9299ca5c36329acfd9f994a5458470e75530e95c
Kayden Hutchinson <56303946+hutch732@users.noreply.github.com>
2022-04-08 11:32:14 -0400

    Add files via upload

--- site.js
+++ site.js
@@ -34,11 +34,59 @@ const vue_app = Vue.createApp({
             // This holds your movies.json data.
             movies: [],
             /* ADD ADDITIONAL VARIABLES FOR STEP 3 HERE */
+            title:"IMDB + Kayden's Top 8 Movies",
+            owner : "Kayden Hutchinson",
+            gitHub : "https://github.com/hutch732/Hutchinson-P3"
       }
     },
       methods: {
             /* ADD FUNCTIONS/METHODS FOR STEP 7 HERE */
-      }
+            getMonthText(dateArray){
+              var month;
+              switch (dateArray[1])
+              {
+              case 1: month = "January";
+              break;
+              case 2: month = "February";
+              break;
+              case 3: month = "March";
+              break;
+              case 4: month = "April";
+              break;
+              case 5: month = "May";
+              break;
+              case 6: month = "June";
+              break;
+              case 7: month = "July";
+              break;
+              case 8: month = "August";
+              break;
+              case 9: month = "September";
+              break;
+              case 10: month = "October";
+              break;
+              case 11: month = "November";
+              break;
+              case 12: month = "December";
+              break;
+              }
+              return month + " " + dateArray[2] + " " + dateArray[0];
+            },
+            posterClick(index) {
+              if (this.movies[index].posterindex >= this.movies[index].posters.length-1) {
+              this.movies[index].posterindex= 0;
+              }
+              else{
+                this.movies[index].posterindex++;
+              }
+            },
+            timeText(minutes) {
+
+  return Math.floor(minutes / 60) + 'h' + ' ' + minutes % 60 + 'm';
+}
+
+
+          }
 })
 
 vue_app.mount("#vue_app")
